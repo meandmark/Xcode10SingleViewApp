18203597c8bf92e91d9eaa2c77b144edd102b061
Mark Szymczyk <mark@meandmark.com>
2018-09-17 21:08:06 -0400

    Initial Commit

---
+++ Xcode10SingleViewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xcode10SingleViewApp
+//
+//  Created by mark on 9/17/18.
+//  Copyright © 2018 Me and Mark Publishing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
